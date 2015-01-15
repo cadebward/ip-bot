578e63eaf1ff6a0e3b60510a58bffbda89c02f6f
Cade Ward <cadebward@gmail.com>
2015-01-14 21:12:28 -0700

    add in ability to post changes to a server

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 var fs = require('fs');
 var request = require('request');
-
-var URL = 'http://bot.whatismyipaddress.com/';
-var FILE_NAME = 'public_ip.txt'
+var config = require('./config');
 
 var CronJob = require('cron').CronJob;
 new CronJob('1 * * * * *', function(){
-    request.get(URL, function(error, response, body) {
-      // body is ip
-      writeIp(body);
+    request.get(config.ip_url, function(error, response, ip) {
+      console.log('checking for ip change');
+      fs.exists(config.file_name, function(exists) {
+        if (exists) {
+          fs.readFile(config.file_name, 'utf8', function(error, currentIp) {
+            if (ip != currentIp) {
+              // ip changed
+              // send post with new ip
+              notify(ip);
+            } else {
+              console.log('ip has not changed');
+            }
+          });
+        }
+      })
     });
 }, null, true, "America/Denver");
 
 function writeIp(ip) {
-  fs.writeFile('./' + FILE_NAME, ip)
+  fs.writeFile(config.file_name, ip)
+  console.log('writing ip to file: ' + ip);
+}
+
+function notify(ip) {
+  writeIp(ip);
+  var nginxServer = config.domain + ':' + config.port;
+  console.log('notify server of new ip: ' + ip);
+  request.post({
+    headers: {'content-type' : 'application/json'},
+    url: 'http://cade.info',
+    json: {ip: ip}
+  }, function(error, response, body){
+    console.log(body);
+    console.log(error);
+    console.log(response.status);
+  });
 }
